e2644210cc70bc14fd79a169407f9234b94dea11
Cameron Garnham <me@da2ce7.com>
2020-09-07 14:50:43 +0800

    autoformat code files

--- 1.2/hello.cpp
+++ 1.2/hello.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
+#include <iostream>
 
-int main(int argc, char **argv) {
-std::cout << "Hello, world!\n";
-return 0;
+int main(int argc, char** argv)
+{
+    std::cout << "Hello, world!\n";
+    return 0;
 }
-
